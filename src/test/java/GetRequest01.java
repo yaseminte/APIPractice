cc27cc8ba4257789ef9df619da0ffcb2ae9e1f96
Yasemin Turker <yturker89@gmail.com>
2022-11-01 23:27:49 +0300

    01.11.2022

---
+++ src/test/java/GetRequest01.java
@@ -0,0 +1,47 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequest01 {
+
+    @Test
+    public void test01() {
+
+        String url = "https://restful-booker.herokuapp.com/booking";
+
+        Response response = given().auth().basic("user","password").when().get(url);
+        // given().when().get(url); end point'e göndermek için request oluşturmuş olduk.
+        // Response response -> api tarafından bana dönen response (cevap)
+        // Response response = given().auth().basic("user","password").when().get(url);
+        // basic aut ile request göndermek için
+
+        // response.prettyPrint();   // response'daki body'i yazdırır
+
+        // response.prettyPeek();       // response'daki her şeyi yazdırır
+
+        // response.peek();          // tüm veriyi string olarak verir.
+
+        // response.print();
+        // [{"bookingid":1215},{"bookingid":844},{"bookingid":87}, ...]
+
+
+        System.out.println(response.statusCode());
+        System.out.println(response.statusLine());
+        System.out.println(response.contentType());
+
+        // 1) JUnit Assert'leri ile API testi yapabiliriz
+        Assert.assertEquals("Status Kod Hatalı",200,response.statusCode());
+        Assert.assertEquals("HTTP/1.1 200 OK",response.statusLine());
+        Assert.assertEquals("application/json; charset=utf-8",response.contentType());
+
+        // 2) assertthat ile
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .statusLine("HTTP/1.1 200 OK");
+
+    }
+}
---
+++ src/test/java/GetRequest03.java
@@ -0,0 +1,13 @@
+public class GetRequest03 {
+
+    /*
+    Matchers ile dataları doğrulayınız
+             "id": 5,
+            "email": "charles.morris@reqres.in",
+            "first_name": "Charles",
+            "last_name": "Morris",
+            "avatar": "https://reqres.in/img/faces/5-image.jpg"
+     */
+
+
+}
